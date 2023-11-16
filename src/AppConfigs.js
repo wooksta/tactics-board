c3bb736b4f1a49eb4f6569d1825d2bda136f3474
Wooksta <toleettobe@gmail.com>
2023-11-16 17:18:00 +0100

    actual fix for home

--- src/AppConfigs.js
+++ src/AppConfigs.js
@@ -8,7 +8,7 @@ const AppConfigs = {
 	},
 	coatchingfutsal: {
 		useFirebase: true,
-		siteHomeURL: '/TacticsBoard',
+		siteHomeURL: '/tactics-board/',
 		siteLogoURL: null,
 		pitchCenterADURL: null,
 		pitchCenterBackground: '/TacticsBoard/field',
